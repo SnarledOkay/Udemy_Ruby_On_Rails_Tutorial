42bacfe44e938fd0c2b115a96089cbe29efd4ee2
Ros Montis <huync0603@gmail.com>
2026-05-15 08:37:31 +0700

    Modify README for github repo demo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
     def hello
     # 'render' just means 'show sth on the screen'
-    render html:'Hello World!'
+    # render html:'Hello World!'
   end
 end
+
+
